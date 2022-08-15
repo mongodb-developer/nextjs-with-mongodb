0c264f465c16594ae40ec16ca40a7004967cffb9
Kushagra Kesav <kushagra198@gmail.com>
2022-08-15 16:06:43 +0530

    Initial Commit

---
+++ pages/movies.js
@@ -0,0 +1,42 @@
+import clientPromise from "../lib/mongodb";
+
+export default function Movies({ movies }) {
+    return (
+        <div>
+            <h1>Top 20 Movies of All Time</h1>
+            <p>
+                <small>(According to Metacritic)</small>
+            </p>
+            <ul>
+                {movies.map((movie) => (
+                    <li>
+                        <h2>{movie.title}</h2>
+                        <h3>{movie._id}</h3>
+                        <p>{movie.plot}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export async function getServerSideProps(context) {
+    try {
+        const client = await clientPromise;
+
+        const db = client.db("sample_mflix");
+
+        const movies = await db
+            .collection("movies")
+            .find({})
+            .sort({ metacritic: -1 })
+            .limit(10)
+            .toArray();
+
+        return {
+            props: { movies: JSON.parse(JSON.stringify(movies)) },
+        };
+    } catch (error) {
+        console.log(error);
+    }
+}
